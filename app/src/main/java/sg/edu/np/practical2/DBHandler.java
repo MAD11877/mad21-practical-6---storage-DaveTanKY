ff6908bd25daa54447e914bcd8e3c98382e0176c
DaveTanKY <dave21sg@gmail.com>
2021-05-31 16:54:03 +0800

    completed practical 6

---
+++ app/src/main/java/sg/edu/np/practical2/DBHandler.java
@@ -0,0 +1,125 @@
+package sg.edu.np.practical2;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+public class DBHandler  extends SQLiteOpenHelper {
+
+    public DBHandler(@Nullable Context context) {
+        super(context, "UserDB", null, 1);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        String createTableStatement = "CREATE TABLE USERS (ID INTEGER PRIMARY KEY AUTOINCREMENT, NAME TEXT, DESCRIPTION TEXT, FOLLOWED INTEGER DEFAULT 0)";
+
+        db.execSQL(createTableStatement);
+
+
+
+        Random rand = new Random();
+        for (int i = 0; i < 20; i++) {
+
+            int randName = rand.nextInt();
+            int randDesc = rand.nextInt();
+            int randFollowed = rand.nextInt();
+            ContentValues values =  new ContentValues();
+
+            user u = new user();
+            u.setName("Dave " + randName);
+            u.setDesc("Description " + randDesc);
+            if (randFollowed % 2 == 0) {
+                values.put("followed", 1);
+            }
+            else {
+                values.put("followed", 0);
+            }
+
+
+            values.put("name", u.name);
+            values.put("description", u.desc);
+
+            db.insert("USERS", null, values);
+        }
+        db.close();
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS USERS");
+        onCreate(db);
+    }
+
+    public void addUser(user u)
+    {
+        ContentValues values = new ContentValues();
+        values.put("name", u.name);
+        values.put("description", u.desc);
+        if (u.followed)
+        {
+            values.put("followed", 1);
+        }
+        else
+        {
+            values.put("followed", 0);
+        }
+
+        SQLiteDatabase db = getWritableDatabase();
+        db.insert("USERS", null, values);
+        db.close();
+    }
+
+    public ArrayList<user> getUser(String name)
+    {
+        SQLiteDatabase db = getWritableDatabase();
+        Cursor cursor = db.rawQuery("SELECT * FROM USERS", null);
+        user u = null;
+        ArrayList<user> list = new ArrayList<>();
+        while(cursor.moveToNext())
+        {
+            u = new user();
+            u.setName(cursor.getString(1));
+            u.setDesc(cursor.getString(2));
+            if (cursor.getInt(3) == 1)
+            {
+                u.setFollowed(true);
+            }
+            else
+            {
+                u.setFollowed(false);
+            }
+            list.add(u);
+        }
+        cursor.close();
+        db.close();
+        return list;
+    }
+
+    public void updateUser(user u)
+    {
+
+        SQLiteDatabase db = getWritableDatabase();
+        ContentValues values = new ContentValues();
+        values.put("name", u.name);
+        values.put("description", u.desc);
+        if (u.followed)
+        {
+            values.put("followed", 1);
+        }
+        else
+        {
+            values.put("followed", 0);
+        }
+
+        db.update("USERS", values, "NAME = ?", new String[]{u.name} );
+    }
+}
---
+++ app/src/main/java/sg/edu/np/practical2/UsersAdapter.java
@@ -0,0 +1,97 @@
+package sg.edu.np.practical2;
+
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Parcelable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+
+public class UsersAdapter extends RecyclerView.Adapter<UserViewHolder> {
+    Context context;
+    ArrayList<user> obj;
+
+
+    public UsersAdapter(Context c, ArrayList d)
+    {
+        context = c;
+        obj = d;
+    }
+
+
+    @NonNull
+    @Override
+    public UserViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View item = null;
+
+        item = LayoutInflater.from(parent.getContext()).inflate(R.layout.vh_user, parent, false);
+        return new UserViewHolder(item);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull UserViewHolder holder, int position) {
+
+        user u = obj.get(position);
+
+        holder.name.setText(u.getName());
+        holder.desc.setText(u.getDesc());
+
+        if(u.getName().endsWith("7"))
+        {
+            holder.bigImage.setVisibility(View.VISIBLE);
+        }
+        else
+        {
+            holder.bigImage.setVisibility(View.GONE);
+        }
+
+        holder.image.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder builder1 = new AlertDialog.Builder(context);
+                builder1.setMessage(u.getName());
+                builder1.setTitle("Profile");
+                builder1.setCancelable(true);
+
+                builder1.setPositiveButton(
+                        "View",
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+
+                                Intent goToMainActivity = new Intent(context, MainActivity.class);
+                                goToMainActivity.putExtra("id", position);
+                                context.startActivity(goToMainActivity);
+                            }
+                        });
+                builder1.setNegativeButton(
+                        "Close",
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                                dialog.cancel();
+                            }
+                        });
+
+                AlertDialog alert11 = builder1.create();
+                alert11.show();
+
+            }
+        });
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return obj.size();
+    }
+
+
+}
